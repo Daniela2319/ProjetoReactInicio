0f881dff91714fec98d345dd1adbc8106cb6b321
Daniela2319 <frankvt04@gmail.com>
2024-07-16 14:15:23 -0300

    atualizando o codigo com react

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,51 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import './styles/App.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Article } from './components/Article/Article';
-import articleImg from './assets/windons.png';
-import article1Img from './assets/windons2 (1).png';
-import article2Img from './assets/windons3.png';
 import { Counter } from './components/Counter/counter';
+import axios from 'axios';
 
 //componente em classe é uma classe que herda a classe component do react,
 //e retorna html dentro do metodo render.
 //componente funcional é uma função que retorna um html
-class App extends React.Component {
+function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get(
+        'https://api.spaceflightnewsapi.net/v3/articles',
+      );
+      const newsData = response.data;
+
+      setNews(newsData);
+    }
+    loadNews();
+  }, []);
+
   //Método responsavel por renderizar o conteudo html do nosso componente
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Counter id="counter" />
-        <section id="articles">
-          <Article
-            title="Designing Dashboard"
-            provider="NASA"
-            description=" Amet minin non deserunt ullamco est sit aliqua dolor do ament sint.
-            Velit officia consequat duis enin velit mollit. Exercitation veniam
-            consequat sunt nostrud amet."
-            thumbnail={articleImg}
-          />
-          <Article
-            title="Vibrant"
-            provider="Spcelles"
-            description=" Amet minin non deserunt ullamco est sit aliqua dolor do ament sint.
-           Velit officia consequat duis enin velit mollit. Exercitation veniam
-           consequat sunt nostrud amet."
-            thumbnail={article1Img}
-          />
-          <Article
-            title="36 Days of Malayalas"
-            provider="Spaceflight"
-            description=" Amet minin non deserunt ullamco est sit aliqua dolor do ament sint.
-           Velit officia consequat duis enin velit mollit. Exercitation veniam
-           consequat sunt nostrud amet."
-            thumbnail={article2Img}
-          />
-          <Article
-            title="Designing Dashboard"
-            provider="NASA"
-            description=" Amet minin non deserunt ullamco est sit aliqua dolor do ament sint.
-           Velit officia consequat duis enin velit mollit. Exercitation veniam
-           consequat sunt nostrud amet."
-            thumbnail={articleImg}
-          />
-        </section>
-      </>
-    );
-  }
+
+  return (
+    <>
+      {/* <Counter /> */}
+      <Navbar />
+      <Counter id="counter" />
+      <section id="articles">
+        {news.map((article) => {
+          return (
+            <Article
+              key={article.id}
+              title={article.title}
+              provider={article.newSite}
+              description={article.summary}
+              thumbnail={article.imageUrl}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import './counter.css';
-export class Counter extends React.Component {
-  constructor() {
-    super();
-    //this.contador = 0;
-    this.state = { contador: 0 };
-  }
-  render() {
-    return (
-      <div id="counter">
-        <h1>{this.state.contador}</h1>
+import { useState } from 'react';
 
-        <div style={{ marginTop: '20px', marginLeft: '20px' }}>
-          <button
-            id="btn"
-            onClick={() => {
-              this.setState({
-                contador: this.state.contador - 1,
-              });
-            }}
-          >
-            DIMINUIR
-          </button>
-          <button
-            id="btn1"
-            onClick={() => {
-              this.setState({
-                contador: this.state.contador + 1,
-              });
-            }}
-          >
-            AUMENTA
-          </button>
-        </div>
+export function Counter() {
+  const [contador, setContador] = useState(0);
+  return (
+    <div id="counter">
+      <h1>{contador}</h1>
+
+      <div style={{ marginTop: '20px', marginLeft: '20px' }}>
+        <button
+          id="btn"
+          onClick={() => {
+            setContador(contador - 1);
+          }}
+        >
+          DIMINUIR
+        </button>
+        <button
+          id="btn1"
+          onClick={() => {
+            setContador(contador + 1);
+          }}
+        >
+          AUMENTA
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
